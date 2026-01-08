0b6aab927506741c9ef0861af8e6f51d4f82af4d
Ekenma Peter <ekenma.peter@gmail.com>
2026-01-08 18:14:24 +0100

    Remove additional forum-related files: Filament, images, theme files, configs

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,19 +10,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-<<<<<<< HEAD
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-=======
-    ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->alias([
-            'admin' => \App\Http\Middleware\AdminMiddleware::class,
-            'moderator' => \App\Http\Middleware\ModeratorMiddleware::class,
-        ]);
-    })
-    ->withExceptions(function (Exceptions $exceptions): void {
->>>>>>> a1fd054322ab54ed5b743f83ff0083053b55df6f
         //
     })->create();
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,4 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-<<<<<<< HEAD
-    App\Providers\Filament\AdminPanelProvider::class,
-=======
->>>>>>> a1fd054322ab54ed5b743f83ff0083053b55df6f
 ];
